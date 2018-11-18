8d59685a5b58ec3a3f2d3c4623a801e01692722b
Oceanwall <matthew853211@gmail.com>
2018-11-18 15:03:28 -0600

    Basic setup for plugin

---
+++ spec/codewall-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodewallView from '../lib/codewall-view';
+
+describe('CodewallView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
